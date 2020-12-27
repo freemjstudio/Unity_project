b34dc7226d9d3a5500110d67adf64a852853028e
freemjstudio <mjwoo0@naver.com>
2020-12-27 21:02:38 +0900

    unity project

---
+++ Assets/EventFunctionsTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventFunctionsTest : MonoBehaviour
+{
+    private void Awake()
+    {
+      Debug.Log("Awake 함수가 실행되었습니다.");
+      Debug.Log("Awake 함수가 실행되었습니다.");
+    }
+
+    private void Start()
+    {
+      Debug.Log("Start함수가 실행되었습니다.");
+    }
+
+    private void OnEnable()
+    {
+      Debug.Log("OnEnable 함수가 실행되었습니다.");
+    }
+
+    private void Update()
+    {
+      Debug.Log("Update 함수가 실행되었씁니다.");
+    }
+
+    private void LateUpdate()
+    {
+      Debug.Log("LateUpdate 함수가 실행되었습니다.");
+    }
+
+    private void OnDestroy()
+    {
+      Debug.Log("OnDestroy 함수가 실행되었습니다.");
+    }
+
+    private void OnApplicationQuit()
+    {
+      Debug.Log("OnApplicationQuit 함수가 실행되었습니다.");
+    }
+}
